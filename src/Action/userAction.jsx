279a8863bd1acf4ef36e664b7ca0f8bf1dc4db2b
Jānis Iesalnieks <iesalnieks.juniors@gmail.com>
2019-08-12 16:14:17 +0300

    init + Submit error handling

---
+++ src/Action/userAction.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const registerUser = async user => {
+  const res = await axios.post("/user/", user);
+  if(res.headers && res.headers.status === 200) {
+      console.log("success");
+  }
+};
+
+export default registerUser;
